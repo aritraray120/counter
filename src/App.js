cda9d3ff21d4093ff99455841c41b10895f2916e
Aritra Ray <aritraray120@gmail.com>
2024-03-09 14:01:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+export default function Counter() {
+ 
+  const [count, setCount] = useState(0);
+
+
+  function incrementCounter() {
+    setCount(count + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Counter: {count}</h2>
+      <button style={{backgroundColor:"blue", color:"white"}} onClick={incrementCounter}>Increment</button>
     </div>
   );
 }
 
-export default App;
